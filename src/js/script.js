2ec33de96e776184d7614059b99f9ff318daebcb
AhmedA <alkhatib_ahmed1995@hotmail.com>
2023-01-30 16:07:28 -0500

    added media queries for desktop design

--- src/js/script.js
+++ src/js/script.js
@@ -1,67 +1,7 @@
-// const bill =
-// const tipPercentage =
-// const numberOfPeople =
-// const tipAmountPerPerson =
-// const totalPerPerson =
-// Tip amount per person = bill X 0.15(15%) / number of people
-// Total per person = bill + tip / number of people
-// Will need to convert tip percentage entered/selected to converted into decimal form for equation
-
-// There is no submit button,
-// The information in the results sections should total as the information is being entered live
-
 // When custom tip is clicked
 // Custom button will transform into input field
 // User will enter custom tip percentage
 // custom button will have additional styles applied when clicked
-
-// Will need to change innerHTML/textContent of tip amount and total result
-
-// Reset button will reset the whole form
-
-// User will enter the total bill
-// User will select a tip percentage
-// User will enter the number of people
-
-// const bill = document.getElementById("bill-input"); ✅
-// const numberOfPeople = document.getElementById("num-of-people"); ✅
-// get the selected tip percentage in a variable ✅
-// const tipPercentage = ✅
-// const totalTip = bill X tipPercentage; ✅
-// const billPlusTip = bill X tipPercentage + bill; ✅
-// tipAmountPerPerson should equal totalTip / numberOfPeople ✅
-// totalCostPerPerson should equal billPlusTip / numberOfPeople ✅
-
-// Global Variables
-// let tipPercentage;
-// const bill = document.getElementById("bill-input");
-// const numberOfPeople = document.getElementById("num-of-people");
-// const totalTip = bill * tipPercentage;
-// const billPlusTip = bill * tipPercentage + bill;
-// const tipAmountPerPerson = totalTip / numberOfPeople;
-// const totalCostPerPerson = billPlusTip / numberOfPeople;
-
-// // Add functionality of tip button percentages into update total function
-
-// // This is listening for which of the percentage buttons is clicked and converting them into a decimal
-// let radioButtons = document.querySelectorAll("input[name='tip-percentage']");
-// for (let i = 0; i < radioButtons.length; i++) {
-//   radioButtons[i].addEventListener("click", function () {
-//     let tipPercentRaw = this.value;
-//     let tipPercentageWhole = parseInt(tipPercentRaw);
-//     let tipPercentage = tipPercentageWhole / 100;
-//   });
-// }
-
-// function updateTotal() {
-//   const tipCost = document.querySelector(".tip-cost");
-//   const totalCost = document.querySelector(".total-cost");
-
-//   tipCost.textContent = tipAmountPerPerson;
-//   totalCost.textContent = totalCostPerPerson;
-// }
-
-// updateTotal();
 
 const billAmountInput = document.querySelector("#bill-input");
 const peopleCountInput = document.querySelector("#num-of-people");
@@ -93,3 +33,17 @@ function calculateTip() {
   tipPerPersonSpan.textContent = "$" + tipPerPerson.toFixed(2);
   totalPerPersonSpan.textContent = "$" + totalPerPerson.toFixed(2);
 }
+
+const resetButton = document.querySelector(".reset-btn");
+
+resetButton.addEventListener("click", function () {
+  billAmountInput.value = "";
+  peopleCountInput.value = "";
+  // customTipPercentage.value = '';
+  // Change this back to false when custom is working?
+  customTipPercentageRadio.checked = true;
+  customTipPercentageRadio.style.display = "block";
+  // customTipPercentage.style.display = 'none';
+  tipPerPersonSpan.textContent = "$0.00";
+  totalPerPersonSpan.textContent = "$0.00";
+});
